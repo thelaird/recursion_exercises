bf0fbc8d8956adebbb3149b2f690907023d5bcbb
thelaird <hey@thisis.lt>
2015-06-23 10:42:16 -0700

    Initial commit

--- lib/recursion_problems.rb
+++ lib/recursion_problems.rb
@@ -2,15 +2,27 @@
 #the sum of the integers.
 
 def sum_recur(array)
-
+  sum = 0
+  if array.empty?
+    0
+  else
+    sum = array.first
+    sum += sum_recur(array.drop(1))
+    sum
+  end
 end
-
 
 #Problem 2: You have array of integers. Write a recursive solution to
 #determine whether or not the array contains a specific value.
 
 def includes?(array, target)
-
+  if array.empty?
+    false
+  elsif array.first == target
+    true
+  else
+    includes?(array.drop(1),target)
+  end
 end
 
 
@@ -18,7 +30,14 @@ end
 #solution to count the number of occurrences of a specific value.
 
 def num_occur(array, target)
-
+  count = 0
+  if array.empty?
+    0
+  else
+    count = 1 if array.first == target
+    count += num_occur(array.drop(1), target)
+    count
+  end
 end
 
 
@@ -26,7 +45,14 @@ end
 #determine whether or not two adjacent elements of the array add to 12.
 
 def add_to_twelve?(array)
- 
+  if array.length < 2
+    false
+  elsif array[0] + array[1] == 12
+    true
+  else
+    add_to_twelve?(array.drop(1))
+  end
+
 end
 
 
@@ -34,7 +60,13 @@ end
 #determine if the array is sorted.
 
 def sorted?(array)
- 
+  if array.length <= 1
+    true
+  elsif array[0] > array[1]
+    false
+  else
+    sorted?(array.drop(1))
+  end
 end
 
 
@@ -42,6 +74,13 @@ end
 #reversed. Must use recursion. (Don't use any #reverse methods!)
 
 def reverse(number)
-
+  digits = number.to_s.chars
+  reversed = []
+  if digits.length <= 1
+    number
+  else
+    reversed << digits.pop
+    reversed << reverse(digits.join.to_i)
+    reversed.join.to_i
+  end
 end
-
